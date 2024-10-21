ba83a44af7178a6d7f701a1f0daab703fb1cf05f
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2024-10-21 12:39:54 +0200

    add endpoint with file name synonyms

--- server/files.js
+++ server/files.js
@@ -2,6 +2,7 @@
 import {deleteOriginalFile, getCurrentDatasetVersion, fileExists, wipeGeneratedFilesAndResetProccessing} from '../util/filesAndDirectories.js'
 import {validate} from './validation.js'
 import {getMimeFromPath} from '../validation/files.js'
+import filenames from '../validation/filenames.js'
 import auth from './Auth/auth.js';
 import config from '../config.js'
 import db from './db/index.js';
@@ -58,9 +59,19 @@ const downloadFile = async (req, res, fromOriginalDir) => {
    
   }
 
+const getFileNameSynonyms = async (req, res) => {
+
+    try {
+        res.json(filenames)
+        } catch (error) {
+            res.sendStatus(500)
+        } 
+}
+
 export default  (app) => {
     app.delete("/dataset/:id/file/:filename", auth.userCanModifyDataset(),  deleteUploadedFile);
     app.get("/dataset/:id/file/:filename", (req, res) => downloadFile(req, res, false))
     app.get("/dataset/:id/uploaded-file/:filename", (req, res) => downloadFile(req, res, true))
+    app.get("/file-name-synonyms", getFileNameSynonyms)
 
 }
