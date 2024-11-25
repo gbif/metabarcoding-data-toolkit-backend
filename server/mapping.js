731bba56a2387264ee66314ec5c326af1bdcc9f5
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2024-11-25 11:47:14 +0100

    fix bug that prevents term mapping to be saved

--- server/mapping.js
+++ server/mapping.js
@@ -1,6 +1,6 @@
 
 import auth from './Auth/auth.js';
-import {writeMapping, getCurrentDatasetVersion} from '../util/filesAndDirectories.js'
+import {writeMapping, readMapping, getCurrentDatasetVersion} from '../util/filesAndDirectories.js'
 
 
 const saveMapping = async function (req, res) {
@@ -12,12 +12,20 @@ const saveMapping = async function (req, res) {
             if(!version){
                 version = await getCurrentDatasetVersion(req.params.id)
             } 
-            console.log(`Save mapping for dataset ${req.params.id}`)
+            const oldMapping = await readMapping(req.params.id, version)
+            const {createdAt, createdBy, ...rest} = oldMapping || {};
+            if(JSON.stringify(rest) !== JSON.stringify(req.body)){
+                console.log(`Save mapping for dataset ${req.params.id}`)
 
-            await writeMapping(req.params.id, version, req.body)
+            await writeMapping(req.params.id, version, {...req.body, createdAt: new Date(), createdBy: req?.user?.userName})
             
            // console.log(eml)
-            res.send(req.body) 
+            res.send(req.body)
+            } else {
+                console.log(`Mapping was not changed for dataset ${req.params.id}`)
+                res.send(req.body)
+            }
+             
         } catch (error) {
             console.log(error)
             res.sendStatus(500)
